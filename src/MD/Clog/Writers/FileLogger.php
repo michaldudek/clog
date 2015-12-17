52767cb34ec044450b59e26e9aedcbb8094ad8eb
Michał Pałys-Dudek <michal@michaldudek.pl>
2015-12-17 15:05:29 +0100

    Create dir for log file.

--- src/MD/Clog/Writers/FileLogger.php
+++ src/MD/Clog/Writers/FileLogger.php
@@ -83,6 +83,11 @@ class FileLogger extends AbstractWriter
         }
 
         if (!is_file($this->filePath)) {
+            $path = dirname($this->filePath);
+            if (is_dir($path)) {
+                mkdir($path, 0777, true);
+            }
+
             touch($this->filePath);
         }
 
